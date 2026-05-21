85dbd26b222a0c5eebfec1caa5e7048fbb08ad9c
Hooni0_0 <startrack0515@gmail.com>
2026-05-22 01:38:40 +0900

    [feat] 어드레서블 KeyContainer 추가

---
+++ NyangNyangStar/Assets/Scripts/Services/AddressableKey/KeyContainer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Services.AddressableKey
+{
+    public static class KeyContainer
+    {
+        public static Dictionary<string, string> KeyDict = new();
+
+        public static string EventSystem = "EventSystem";
+
+        public static bool GetAddressableKey(string key)
+        {
+            if(KeyDict[key] == key)
+                return true;
+
+            DebugTool.Log("존재 하지 않는 키 입니다.", DebugType.Addressable);
+            return false;
+        }
+
+        public static void InitKeyDict()
+        {
+            KeyDict.Clear();
+            KeyDict.Add(EventSystem, EventSystem);
+            
+            DebugTool.Log("KeyContainer InitKeyDict", DebugType.Addressable);
+        }
+    }
+}
