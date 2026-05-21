5c68146bff48ab984689f345058e6aa7f288ea9a
제갈도원 <insopa01@naver.com>
2026-05-21 22:11:17 +0900

    fix : Addressable Test
    - 이벤트 시스템 비동기 연결

--- NyangNyangStar/Assets/Scripts/Core/Scenes/BaseScene.cs
+++ NyangNyangStar/Assets/Scripts/Core/Scenes/BaseScene.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class BaseScene : MonoBehaviour
 {
+    private const string EventSystemAddress = "EventSystem";
+
+
     void Awake() => Init();
 
     protected virtual void Init()
@@ -11,9 +16,27 @@ public class BaseScene : MonoBehaviour
 
         if (obj == null)
         {
-            // TODO : 이벤트 시스템을 어드레서블로 불러오기
-            // GameObject prefab = GameManager.Resource.Instantiate("EventSystem");
-            // DontDestroyOnLoad(prefab);
+            LoadEventSystem();
         }
+    }
+
+    protected void LoadEventSystem()
+    {
+        // 어드레서블에 동록된 EventSystem 프래팹을 비동기 생성
+        Addressables.InstantiateAsync(EventSystemAddress).Completed += handle =>
+        {
+            // 로드 및 생성 성공
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                // EventSystem 가져오기
+                GameObject eventSystem = handle.Result;
+
+                DontDestroyOnLoad(eventSystem);
+            }
+            else
+            {
+                DebugTool.Log($"EventSystem Addresable 로드 실패", DebugType.Missing);
+            }
+        };
     }
 }
