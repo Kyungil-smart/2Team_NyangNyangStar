382c6f4de103dfbab178dde706615d4686b9c63c
Hooni0_0 <startrack0515@gmail.com>
2026-05-22 01:50:06 +0900

    [feat] AddressableManager 추가
    - AddressableManager 신규 추가
    - AddressableKey 검증 후 프리팹 로드 처리 추가
    - Addressables.InstantiateAsync 기반 비동기 프리팹 생성 로직 추가
    - 로드된 프리팹 DontDestroyOnLoad 처리 추가
    - @Addressable 루트 오브젝트 생성 및 유지 처리 추가
    - KeyContainer 초기화 로직 추가
    - Init, Clear 기반 어드레서블 매니저 생명주기 처리 추가
    - 어드레서블 초기화, 제거, 로드 실패 로그 추가

---
+++ NyangNyangStar/Assets/Scripts/Core/Managers/Addressable Manager.cs
@@ -0,0 +1,63 @@
+﻿using Services.AddressableKey;
+using Services.Scriptable_Object;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Core.Managers
+{
+    public class AddressableManager : ISubManager
+    {
+        private GameObject _root;
+
+        public bool TryLoadPrefab(string key)
+        {
+            if (!KeyContainer.GetAddressableKey(key))
+                return false;
+            
+            GameObject prefab = null;
+            Addressables.InstantiateAsync(key).Completed += handle =>
+            {
+                // 로드 및 생성 성공
+                if (handle.Status == AsyncOperationStatus.Succeeded)
+                {
+                    // EventSystem 가져오기
+                    prefab = handle.Result;
+
+                    Object.DontDestroyOnLoad(prefab);
+                }
+                else
+                {
+                    DebugTool.Log($"{key} 로드 실패", DebugType.Missing);
+                }
+            };
+
+            return true;
+        }
+        
+        public void Init()
+        {
+            KeyContainer.InitKeyDict();
+            
+            _root = GameObject.Find("@Addressable");
+            
+            if (_root == null)
+            {
+                _root = new GameObject { name = "@Addressable" };
+                Object.DontDestroyOnLoad(_root);
+            }
+
+            DebugTool.Log("어드레서블 매니저 초기화 완료", DebugType.Game);
+        }
+
+        public void Clear()
+        {
+            if (_root == null) return;
+            
+            Object.Destroy(_root);
+            _root = null;
+            
+            DebugTool.Log("어드레서블 매니저 제거 완료", DebugType.Game);
+        }
+    }
+}
