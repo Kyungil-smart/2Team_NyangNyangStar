327b3bbbf808dc4d999fe0a08e1ab7c0d431375d
Hooni0_0 <startrack0515@gmail.com>
2026-05-24 22:33:16 +0900

    [refactor] BaseScene EventSystem 로드 방식 수정
    - BaseScene의 EventSystem 로드 호출을 LoadPrefab 방식으로 변경
    - 기존 TryLoadPrefab 호출 코드 제거
    - AddressableManager 로드 메서드 변경 사항 반영
    - EventSystem 로드 성공 및 실패 콜백 구조 유지

--- NyangNyangStar/Assets/Scripts/Services/AddressableKey/KeyContainer.cs
+++ NyangNyangStar/Assets/Scripts/Services/AddressableKey/KeyContainer.cs
@@ -1,28 +1,65 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Services.AddressableKey
 {
     public static class KeyContainer
     {
-        public static Dictionary<string, string> KeyDict = new();
+        private static Dictionary<string, List<GameObject>> KeyDict = new();
 
-        public static string EventSystem = "EventSystem";
+        public static readonly string EventSystem = "EventSystem";
+        public static readonly string TestUI = "TestScene";
+        public static readonly string PopupUI = "UITestPopup";
 
-        public static bool GetAddressableKey(string key)
+        public static bool IsContainsKey(string key)
         {
-            if(KeyDict[key] == key)
-                return true;
-
-            DebugTool.Log("존재 하지 않는 키 입니다.", DebugType.Addressable);
-            return false;
+            if (!KeyDict.ContainsKey(key))
+            {
+                DebugTool.Log("존재 하지 않는 키 입니다.", DebugType.Addressable);
+                return false;
+            }
+            return true;
         }
+
+        public static void AddPrefab(string key, GameObject prefab)
+        {
+            KeyDict[key].Add(prefab);
+            DebugTool.Log($"{key} :  Prefab : {prefab.name}", DebugType.Addressable);
+        }
+
+        public static void RemovePrefab(string key, GameObject prefab)
+            => KeyDict[key].Remove(prefab);
+
+        public static bool IsPrefabActive(string key, GameObject go)
+        {
+            if (!KeyDict.ContainsKey(key))
+                return false;
+
+            if (!KeyDict[key].Contains(go))
+            {
+                DebugTool.Log("해당 프리팹이 존재하지 않습니다.", DebugType.Addressable);
+                return false;
+            }
+
+            return true;
+        }
+        
+        
 
         public static void InitKeyDict()
         {
             KeyDict.Clear();
-            KeyDict.Add(EventSystem, EventSystem);
             
-            DebugTool.Log("KeyContainer InitKeyDict", DebugType.Addressable);
+            KeyDict.Add(EventSystem, new List<GameObject>());
+            KeyDict.Add(TestUI, new List<GameObject>());
+            KeyDict.Add(PopupUI, new List<GameObject>());
+
+            string log = "[어드레서블 키 딕셔너리 초기화]";
+
+            foreach (string key in KeyDict.Keys)
+                log += $"\n{key}";
+
+            DebugTool.Log($"{log}", DebugType.Addressable);
         }
     }
 }
