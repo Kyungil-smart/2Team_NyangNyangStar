cfa9180858c7368ca7ce9aa0aa7b756e77bfc01c
Hooni0_0 <startrack0515@gmail.com>
2026-05-24 22:18:05 +0900

    [fix] DebugTool 멀티라인 색상 로그 처리 수정
    멀티라인 로그를 줄 단위로 색상 처리
    Unity Console에서 color 태그가 노출되는 문제 수정
    로그 출처 출력 방식 유지

--- NyangNyangStar/Assets/Scripts/Util/Debug/DebugTool.cs
+++ NyangNyangStar/Assets/Scripts/Util/Debug/DebugTool.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 using Object = UnityEngine.Object;
@@ -136,8 +137,25 @@ public static class DebugTool
         string source = $"{sourceName}.{safeMemberName} : {Mathf.Max(1, lineNumber)}";
         string levelText = level == DebugLogLevel.Log ? string.Empty : $"/{level}";
 
-        return $"<color={color}>[{type}{levelText}] {text}</color>\n" +
-               $"<color=#DAA520>출처 : [{source}]</color>";
+        string normalizedText = text.Replace("\r\n", "\n");
+        string[] lines = normalizedText.Split('\n');
+
+        StringBuilder builder = new();
+
+        builder.Append($"<color={color}>[{type}{levelText}]</color> ");
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i > 0)
+                builder.AppendLine();
+
+            builder.Append($"<color={color}>{lines[i]}</color>");
+        }
+
+        builder.AppendLine();
+        builder.Append($"<color=#DAA520>출처 : [{source}]</color>");
+
+        return builder.ToString();
     }
 
     private static string GetSourceName(Object context, string filePath)
