723b12b471ea1f70458f7fbc5ad3821391a4ca18
Hooni0_0 <startrack0515@gmail.com>
2026-05-26 02:20:43 +0900

    [refactor] KeyContainer 어드레서블 키 분류 구조 정리
    - KeyContainer의 어드레서블 키 관리 구조 분리
    - Prefab, Audio, Image 키를 내부 static 클래스로 분류
    - Prefab 키 Dictionary를 별도 prefabKeyDict로 변경
    - Audio 키를 HashSet 기반으로 관리하도록 추가
    - Sprite 키를 HashSet 기반으로 관리하는 구조 추가
    - Prefab, Audio, Sprite 키 검증 메서드 분리
    - 생성된 Prefab 등록 및 제거 로직을 prefabKeyDict 기준으로 수정
    - Key 초기화 메서드를 InitKeys 기준으로 정리
    - 초기화된 어드레서블 키 목록 출력 로그 개선

--- NyangNyangStar/Assets/Scripts/Services/AddressableKey/KeyContainer.cs
+++ NyangNyangStar/Assets/Scripts/Services/AddressableKey/KeyContainer.cs
@@ -1,21 +1,63 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace Services.AddressableKey
 {
     public static class KeyContainer
     {
-        private static Dictionary<string, List<GameObject>> KeyDict = new();
+        private static Dictionary<string, List<GameObject>> prefabKeyDict = new();
+        private static HashSet<string> audioClips = new();
+        private static HashSet<string> sprites = new();
 
-        public static readonly string EventSystem = "EventSystem";
-        public static readonly string TestUI = "TestScene";
-        public static readonly string PopupUI = "UITestPopup";
-
-        public static bool IsContainsKey(string key)
+        public static class Prefabs
         {
-            if (!KeyDict.ContainsKey(key))
+            public const string EventSystem = "EventSystem";
+            public const string TestUI = "TestScene";
+            public const string VolumePopupUI = "VolumePopupUI";
+            public const string BGMPopupUI = "BGMPopupUI";
+        }
+
+        public static class Audio
+        {
+            public const string TitleBGM = "BGM/Title.mp3";
+            public const string BGM01 = "BGM/BGM 01.mp3";
+            public const string BGM02 = "BGM/BGM 02.mp3";
+            public const string BGM03 = "BGM/BGM 03.mp3";
+        }
+
+        public static class Image
+        {
+            
+        }
+
+        public static bool IsPrefabKey(string key)
+        {
+            if (!prefabKeyDict.ContainsKey(key))
             {
-                DebugTool.Log("존재 하지 않는 키 입니다.", DebugType.Addressable);
+                DebugTool.Log($"{key} : 존재 하지 않는 Prefab Key 입니다.", DebugType.Addressable);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsAudioKey(string key)
+        {
+            if (!audioClips.Contains(key))
+            {
+                DebugTool.Log($"{key} : 존재 하지 않는 Audio Key 입니다.", DebugType.Addressable);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsSpriteKey(string key)
+        {
+            if (!sprites.Contains(key))
+            {
+                DebugTool.Log($"{key} : 존재 하지 않는 Image Key 입니다.", DebugType.Addressable);
                 return false;
             }
             return true;
@@ -23,19 +65,19 @@ namespace Services.AddressableKey
 
         public static void AddPrefab(string key, GameObject prefab)
         {
-            KeyDict[key].Add(prefab);
+            prefabKeyDict[key].Add(prefab);
             DebugTool.Log($"{key} :  Prefab : {prefab.name}", DebugType.Addressable);
         }
 
         public static void RemovePrefab(string key, GameObject prefab)
-            => KeyDict[key].Remove(prefab);
+            => prefabKeyDict[key].Remove(prefab);
 
         public static bool IsPrefabActive(string key, GameObject go)
         {
-            if (!KeyDict.ContainsKey(key))
+            if (!prefabKeyDict.ContainsKey(key))
                 return false;
 
-            if (!KeyDict[key].Contains(go))
+            if (!prefabKeyDict[key].Contains(go))
             {
                 DebugTool.Log("해당 프리팹이 존재하지 않습니다.", DebugType.Addressable);
                 return false;
@@ -43,23 +85,55 @@ namespace Services.AddressableKey
 
             return true;
         }
-        
-        
 
-        public static void InitKeyDict()
+        public static void InitKeys()
         {
-            KeyDict.Clear();
+            PrefabKeyDictInit();
+            AudioClipsInit();
+            ImagesInit();
             
-            KeyDict.Add(EventSystem, new List<GameObject>());
-            KeyDict.Add(TestUI, new List<GameObject>());
-            KeyDict.Add(PopupUI, new List<GameObject>());
+            PrintKeys();
+        }
 
-            string log = "[어드레서블 키 딕셔너리 초기화]";
+        private static void PrefabKeyDictInit()
+        {
+            prefabKeyDict.Clear();
 
-            foreach (string key in KeyDict.Keys)
-                log += $"\n{key}";
+            prefabKeyDict.Add(Prefabs.EventSystem, new List<GameObject>());
+            prefabKeyDict.Add(Prefabs.TestUI, new List<GameObject>());
+            prefabKeyDict.Add(Prefabs.VolumePopupUI, new List<GameObject>());
+            prefabKeyDict.Add(Prefabs.BGMPopupUI, new List<GameObject>());
+        }
 
-            DebugTool.Log($"{log}", DebugType.Addressable);
+        private static void AudioClipsInit()
+        {
+            audioClips.Clear();
+            
+            audioClips.Add(Audio.TitleBGM);
+            audioClips.Add(Audio.BGM01);
+            audioClips.Add(Audio.BGM02);
+            audioClips.Add(Audio.BGM03);
+        }
+
+        private static void ImagesInit()
+        {
+            sprites.Clear();
+        }
+
+        private static void PrintKeys()
+        {
+            StringBuilder log = new();
+            log.AppendLine("[어드레서블 키 초기화]");
+
+            log.AppendLine("[Prefabs]");
+            foreach (string key in prefabKeyDict.Keys)
+                log.AppendLine(key);
+
+            log.AppendLine("[Audio]");
+            foreach (string key in audioClips)
+                log.AppendLine(key);
+
+            DebugTool.Log(log.ToString(), DebugType.Addressable);
         }
     }
 }
