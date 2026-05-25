34a99e5a8032a234c09039e42db104fedf7535c7
Hooni0_0 <startrack0515@gmail.com>
2026-05-26 02:18:12 +0900

    [feat] UIPopup Addressable Key 저장 기능 추가
    - UIPopup에 AddressableKey 프로퍼티 추가
    - 팝업 생성 시 사용한 Addressable Key 저장 구조 추가
    - SetAddressableKey 메서드 추가
    - 팝업 해제 시 등록된 Addressable Key를 사용할 수 있도록 기반 구조 추가

--- NyangNyangStar/Assets/Scripts/UI/UIPopup.cs
+++ NyangNyangStar/Assets/Scripts/UI/UIPopup.cs
@@ -4,6 +4,10 @@ namespace UI
 {
     public class UIPopup : UIBase
     {
+        public string AddressableKey { get; private set; }
+
+        public void SetAddressableKey(string key)
+            => AddressableKey = key;
         public override void Init() { }
         
         public virtual void ClosePopup()
