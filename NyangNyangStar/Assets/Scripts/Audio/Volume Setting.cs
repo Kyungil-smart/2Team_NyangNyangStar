6a1c90a668adc7eb6c9a9c494b7297ac01644819
Hooni0_0 <startrack0515@gmail.com>
2026-05-26 02:24:15 +0900

    [refactor] VolumeSetting UIBase 바인딩 구조 적용
    - VolumeSetting이 UIBase를 상속하도록 변경
    - Awake에서 Init 호출 추가
    - Button 수동 참조를 Bind 기반 자동 바인딩으로 변경
    - BGM, SFX 버튼 enum 추가
    - Get<Button>을 통해 버튼 참조를 초기화하도록 수정

--- NyangNyangStar/Assets/Scripts/Audio/Volume Setting.cs
+++ NyangNyangStar/Assets/Scripts/Audio/Volume Setting.cs
@@ -1,14 +1,21 @@
 using Core.Managers;
+using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Audio
 {
-    public class VolumeSetting : MonoBehaviour
+    public class VolumeSetting : UIBase
     {
         [Space(8)] [Header("오디오 설정 버튼")]
         [SerializeField] private Button _bgmButton;
         [SerializeField] private Button _sfxButton;
+        
+
+        private void Awake()
+        {
+            Init();
+        }
         
         private void OnEnable()
         {
@@ -38,5 +45,19 @@ namespace Audio
             GameManager.Audio.SetSFXState();
             DebugTool.Log($"SFX 볼륨 변경 : {GameManager.Audio.SfxOnOff}", DebugType.UI, this);
         }
+
+        public override void Init()
+        {
+            Bind<Button>(typeof(AudioSettingButtons));
+            
+            _bgmButton = Get<Button>((int)AudioSettingButtons.BGMButton);
+            _sfxButton = Get<Button>((int)AudioSettingButtons.SFXButton);
+        }
+    }
+
+    public enum AudioSettingButtons
+    {
+        BGMButton,
+        SFXButton
     }
 }
