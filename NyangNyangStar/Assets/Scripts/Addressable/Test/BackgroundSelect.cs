093d001e7cb256eab228fe8bf37a71e540e9ae52
Hooni0_0 <startrack0515@gmail.com>
2026-05-26 17:16:02 +0900

    [refactor] BackgroundSelect Sprite 제어 구조 분리
    - BackgroundSelect의 Sprite 로드 및 해제 로직 제거
    - UISpriteController 기반으로 배경 Sprite 변경 처리 위임
    - Init에서 Background Image 참조 및 UISpriteController 생성 처리 추가
    - OnDestroy 시 UISpriteController Dispose 처리 추가
    - BackgroundController 접근 프로퍼티 추가

--- NyangNyangStar/Assets/Scripts/Addressable/Test/BackgroundSelect.cs
+++ NyangNyangStar/Assets/Scripts/Addressable/Test/BackgroundSelect.cs
@@ -1,61 +1,33 @@
-using Core.Managers;
 using Services.AddressableKey;
 using UI;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 
 public class BackgroundSelect : UIBase
 {
     [SerializeField] private Image _backgroundImage;
-    private Sprite _sprite;
-    private AsyncOperationHandle<Sprite> _handle;
+
+    private UISpriteController _backgroundController;
+    public UISpriteController BackgroundController => _backgroundController;
 
     private void Awake()
-        => _backgroundImage = GetComponentInChildren<Image>();
+        => Init();
 
     private void Start()
     {
-        ChangeSprite(KeyContainer.Sprite.Title);
-    }
-
-    public void ChangeSprite(string key)
-    {
-        GameManager.Addressable.LoadSprite(key,
-            (loadedSprite, handle) =>
-            {
-                ReleaseSprite();
-
-                _handle = handle;
-                _sprite = loadedSprite;
-                _backgroundImage.sprite = _sprite;
-            },
-            failedKey =>
-            {
-                DebugTool.Warning($"{failedKey} : Sprite 로드 실패", DebugType.UI);
-            });
-    }
-
-    private void ReleaseSprite()
-    {
-        if (!_handle.IsValid())
-            return;
-        
-        Addressables.Release(_handle);
-        
-        _handle = default;
-        _sprite = null;
-        
-        DebugTool.Log("기존 Sprite Release 완료", DebugType.Addressable);
+        _backgroundController.ChangeSprite(KeyContainer.Sprite.Title);
     }
 
     private void OnDestroy()
     {
-        ReleaseSprite();
+        _backgroundController?.Dispose();
     }
 
     public override void Init()
     {
+        if(_backgroundImage == null)
+            _backgroundImage = GetComponentInChildren<Image>();
+        
+        _backgroundController = new(_backgroundImage);
     }
 }
