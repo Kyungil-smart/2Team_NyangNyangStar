d7df44345e260f3c82d59fcd0356e5ef5dd07653
Hooni0_0 <startrack0515@gmail.com>
2026-05-27 15:54:05 +0900

    [refactor] SheetData 네임스페이스 및 로드 상태 추가
    - SheetData를 Data.Parsing 네임스페이스로 이동
    - SheetType enum을 Data.Parsing 내부로 이동
    - SheetData에 loadComplete 상태값 추가
    - 시트 로드 완료 시 loadComplete 갱신 처리 추가
    - Google Sheet 로드 코드 구조 정리

--- NyangNyangStar/Assets/Scripts/Data/Parsing/SheetData.cs
+++ NyangNyangStar/Assets/Scripts/Data/Parsing/SheetData.cs
@@ -3,62 +3,60 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
-
-[System.Serializable]
-public struct SheetData
+namespace Data.Parsing
 {
-    [field: SerializeField] public string URL { get; private set; }
-    [field: SerializeField] public SheetType Type { get; private set; }
-    public char SplitSymbol => Type == SheetType.CSV ? ',' : '\t';
-
-    public SheetData(string url, SheetType type)
+    [Serializable]
+    public struct SheetData
     {
-        URL = url;
-        Type = type;
-    }
+        [field: SerializeField] public string URL { get; private set; }
+        [field: SerializeField] public SheetType Type { get; private set; }
+        public char SplitSymbol => Type == SheetType.CSV ? ',' : '\t';
 
-    public IEnumerator Load(Action<char, string[]> SuccessCallback)
-    {
+        public bool loadComplete;
 
-
-        string sheetId = URL.Split("d/")[1].Split('/')[0];
-
-        string gid = URL.Split("gid=")[1].Split('&')[0].Split('#')[0];
-
-
-        string format = Type == SheetType.CSV ? "csv" : "tsv";
-
-
-        string exportURL = $"https://docs.google.com/spreadsheets/d/{sheetId}/export?format={format}&gid={gid}";
-
-        using (UnityWebRequest uwr = UnityWebRequest.Get(exportURL))
+        public SheetData(string url, SheetType type)
         {
+            URL = url;
+            Type = type;
+            loadComplete = false;
+        }
 
-            yield return uwr.SendWebRequest();
+        public IEnumerator Load(Action<char, string[]> SuccessCallback)
+        {
+            string sheetId = URL.Split("d/")[1].Split('/')[0];
 
-            if (uwr.result != UnityWebRequest.Result.Success)
+            string gid = URL.Split("gid=")[1].Split('&')[0].Split('#')[0];
+
+            string format = Type == SheetType.CSV ? "csv" : "tsv";
+
+            string exportURL = $"https://docs.google.com/spreadsheets/d/{sheetId}/export?format={format}&gid={gid}";
+
+            using (UnityWebRequest uwr = UnityWebRequest.Get(exportURL))
             {
-                DebugTool.Error($"Failed to load sheet data from {exportURL} : {uwr.error}", DebugType.Data);
 
-                yield break;
+                yield return uwr.SendWebRequest();
+
+                if (uwr.result != UnityWebRequest.Result.Success)
+                {
+                    DebugTool.Error($"Failed to load sheet data from {exportURL} : {uwr.error}", DebugType.Data);
+
+                    yield break;
+                }
+
+                string sheetDataText = uwr.downloadHandler.text;
+
+                string[] lines = sheetDataText.Split('\n');
+
+                SuccessCallback?.Invoke(SplitSymbol, lines);
+                loadComplete = true;
+                DebugTool.Log($"Successfully loaded sheet data from {exportURL}", DebugType.Data);
             }
-
-            string sheetDataText = uwr.downloadHandler.text;
-
-
-            string[] lines = sheetDataText.Split('\n');
-
-
-            SuccessCallback?.Invoke(SplitSymbol, lines);
-            DebugTool.Log($"Successfully loaded sheet data from {exportURL}", DebugType.Data);
         }
     }
-
-    
-}
-public enum SheetType
-{
-    CSV,
-    JSON,
-    TSV
+    public enum SheetType
+    {
+        CSV,
+        JSON,
+        TSV
+    }
 }
