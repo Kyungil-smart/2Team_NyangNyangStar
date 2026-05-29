07016400984080d266dc5dc72dad24eb263d11c5
Hooni0_0 <startrack0515@gmail.com>
2026-05-29 15:39:32 +0900

    [feat] ScratchingTime 데이터 SO 추가
    - ScratchingTime ScriptableObject 에셋 추가
    - ScratchingData 데이터 클래스 추가
    - ScratchingSheetLoadSo 시트 로드 구조 추가
    - 스테이지별 일일/주간 내구도 및 클리어 경험치 필드 정의

---
+++ NyangNyangStar/Assets/Scripts/Data/ScriptableObjects/ScratchingTimeSO/ScratchingSheetLoadSo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data.ScriptableObjects.ScratchingTimeSO
+{
+    [CreateAssetMenu(fileName = "ScratchingTime", menuName = "SO/Data/ScratchingTimeSO", order = 0)]
+    public class ScratchingSheetLoadSo : SheetLoadSoBase, ISheetParsable
+    {
+        [SerializeField] private List<ScratchingData> _scratchingDatas = new();
+        public List<ScratchingData> ScratchingDatas => _scratchingDatas;
+
+        private Dictionary<int, ScratchingData> _dataDict = new();
+        
+        public override void Init()
+        {
+            ClearData();
+        }
+        
+        public void ClearData()
+        {
+            _scratchingDatas.Clear();
+        }
+
+        public void SetData(string[] cols)
+        {
+            // ScratchingData data = new {cols[0].Trim(), int.TryParse(cols[1], out int result),  int.Parse(cols[2]), int.Parse(cols[3]), int.Parse(cols[4])};
+        }
+    }
+}
