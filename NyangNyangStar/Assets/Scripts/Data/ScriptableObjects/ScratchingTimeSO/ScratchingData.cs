f11dde0c35eb16ba6dc7df699249c41858243d3b
Hooni0_0 <startrack0515@gmail.com>
2026-05-29 18:17:03 +0900

    [refactor] ScratchingData 캡슐화 구조 변경
    - ScratchingData 필드명에 언더스코어 적용
    - ScratchingData 프로퍼티 setter를 private으로 변경
    - 외부에서 데이터 값을 직접 변경하지 못하도록 제한
    - 생성자를 통해 ScratchingData 초기값을 설정하도록 변경

--- NyangNyangStar/Assets/Scripts/Data/ScriptableObjects/ScratchingTimeSO/ScratchingData.cs
+++ NyangNyangStar/Assets/Scripts/Data/ScriptableObjects/ScratchingTimeSO/ScratchingData.cs
@@ -6,15 +6,26 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
     [Serializable]
     public class ScratchingData
     {
-        [SerializeField] private string stage;
-        public string Stage { get => stage; set => stage = value; }
-        [SerializeField] private int dailyDurability;
-        public int DailyDurability { get => dailyDurability; set => dailyDurability = value; }
-        [SerializeField] private int dailyClearExp;
-        public int DailyClearExp { get => dailyClearExp; set => dailyClearExp = value; }
-        [SerializeField] private int weeklyDurability;
-        public int WeeklyDurability { get => weeklyDurability; set => weeklyDurability = value; }
-        [SerializeField] private int weeklyClearExp;
-        public int WeeklyClearExp { get => weeklyClearExp; set => weeklyClearExp = value; }
+        [SerializeField] private string _stage;
+        public string Stage { get => _stage; private set => _stage = value; }
+        [SerializeField] private int _dailyDurability;
+        public int DailyDurability { get => _dailyDurability; private set => _dailyDurability = value; }
+        [SerializeField] private int _dailyClearExp;
+        public int DailyClearExp { get => _dailyClearExp; private set => _dailyClearExp = value; }
+        [SerializeField] private int _weeklyDurability;
+        public int WeeklyDurability { get => _weeklyDurability; private set => _weeklyDurability = value; }
+        [SerializeField] private int _weeklyClearExp;
+        public int WeeklyClearExp { get => _weeklyClearExp; private set => _weeklyClearExp = value; }
+        
+        public ScratchingData(string stage, 
+            int dailyDurability, int dailyClearExp, 
+            int weeklyDurability, int weeklyClearExp)
+        {
+            _stage = stage;
+            _dailyDurability = dailyDurability;
+            _dailyClearExp = dailyClearExp;
+            _weeklyDurability = weeklyDurability;
+            _weeklyClearExp = weeklyClearExp;
+        }
     }
 }
