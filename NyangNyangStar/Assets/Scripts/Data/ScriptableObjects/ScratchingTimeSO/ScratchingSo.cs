8267c8228aa830799fbe755e1b8b9222ecc3186c
Hooni0_0 <startrack0515@gmail.com>
2026-05-31 22:47:02 +0900

    [refactor] Scratching 데이터 내구도 참조 구조 변경
    - ScratchingData 리스트 필드명 단수형으로 변경
    - 일일/주간 내구도 조회 대상을 최대 내구도로 변경
    - Scratching 데이터 출력 메서드명을 PrintData로 변경
    - Scratching 데이터 출력 로그에서 최대 내구도 기준으로 표시

--- NyangNyangStar/Assets/Scripts/Data/ScriptableObjects/ScratchingTimeSO/ScratchingSo.cs
+++ NyangNyangStar/Assets/Scripts/Data/ScriptableObjects/ScratchingTimeSO/ScratchingSo.cs
@@ -9,8 +9,8 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
     {
         [Header("스크래칭 타임 스테이지 데이터")]
         [Tooltip("단계, 일일 내구도, 일일 경험치, 주간 내구도, 주간 경험치에 대한 정보")]
-        [SerializeField] private List<ScratchingData> _scratchingDatas = new();
-        public List<ScratchingData> ScratchingDatas => _scratchingDatas;
+        [SerializeField] private List<ScratchingData> _scratchingData = new();
+        public List<ScratchingData> ScratchingData => _scratchingData;
 
         private Dictionary<int, ScratchingData> _dataDict = new();
         private int _count = 1;
@@ -20,7 +20,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
         
         public void ClearData()
         {
-            _scratchingDatas.Clear();
+            _scratchingData.Clear();
             _dataDict.Clear();
         }
 
@@ -30,7 +30,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
                     int.Parse(cols[1].Trim()), int.Parse(cols[2].Trim()),
                     int.Parse(cols[3].Trim()), int.Parse(cols[4].Trim()));
             
-            _scratchingDatas.Add(data);
+            _scratchingData.Add(data);
             _dataDict.Add(_count++, data);
         }
 
@@ -43,7 +43,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
             if (!IsContainsKey(stage))
                 return 0;
 
-            return _dataDict[stage].DailyDurability;
+            return _dataDict[stage].MaxDailyDurability;
         }
 
         /// <summary>
@@ -66,7 +66,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
         {
             if (!IsContainsKey(stage))
                 return 0;
-            return _dataDict[stage].WeeklyDurability;
+            return _dataDict[stage].MaxWeeklyDurability;
         }
 
         /// <summary>
@@ -95,17 +95,17 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
             return true;
         }
 
-        public void PrintDatas()
+        public void PrintData()
         {
             StringBuilder log = new();
             log.AppendLine("[ScratchingTimeSO 로드 완료]");
 
-            foreach (ScratchingData data in _scratchingDatas)
+            foreach (ScratchingData data in _scratchingData)
             {
                 log.AppendLine($"[{data.Stage}] " +
-                           $"일일 내구도 = {data.DailyDurability}, " +
+                           $"일일 내구도 = {data.MaxDailyDurability}, " +
                            $"일일 경험치 = {data.DailyClearExp}, " +
-                           $"주간 내구도 = {data.WeeklyDurability}, " +
+                           $"주간 내구도 = {data.MaxWeeklyDurability}, " +
                            $"주간 경험치 = {data.WeeklyClearExp}");
             }
             
