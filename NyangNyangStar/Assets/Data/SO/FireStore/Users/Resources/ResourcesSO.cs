5e261765eb224c84264c114279aaba41383dbf2d
lees2541 <suhyeonglee@naver.com>
2026-06-01 09:41:41 +0900

    [feat] API를 위한 매퍼, 어트리뷰트 추가

--- NyangNyangStar/Assets/Data/SO/FireStore/Users/Resources/ResourcesSO.cs
+++ NyangNyangStar/Assets/Data/SO/FireStore/Users/Resources/ResourcesSO.cs
@@ -1,13 +1,9 @@
-using Firebase.Firestore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
-using static UnityEngine.UIElements.UxmlAttributeDescription;
-[CreateAssetMenu(fileName = "ResourcesSO", menuName = "ScriptableObjects/ResourcesSO")]
 
+[FirestorePath("Users/{userId}/Resources/{docId}")]
+[CreateAssetMenu(fileName = "ResourcesSO", menuName = "ScriptableObjects/ResourcesSO")]
 public class ResourcesSO : BaseFireStore
 {
-    private string userId;
     [SerializeField] public int energy = 200;
     [SerializeField] public int coin = 100;
     [SerializeField] public int jewel = 100;
@@ -18,90 +14,4 @@ public class ResourcesSO : BaseFireStore
     [SerializeField] public int totalGottenJewel = 0;
 
     [SerializeField] public int cumulativeMerge = 0;
-
-    public override async Task CreateNew(FirebaseFirestore database)
-    {
-        this.db = database;
-
-        Dictionary<string, object> data = new Dictionary<string, object>
-        {
-            { "Energy", energy },
-            { "Coin", coin },
-            { "Jewel", jewel }
-        };
-        await SetDataAsync(data);
-    }
-
-    public override async Task CreateNew(FirebaseFirestore database, string userId)
-    {
-        this.db = database;
-        this.userId = userId;
-
-
-        Dictionary<string, object> data = new Dictionary<string, object>
-        {
-            { "Energy", 200 },
-            { "Coin", 100 },
-            { "Jewel", 100 }
-        };
-        await SetDataAsync(data);
-    }
-
-    public override void InitDataBase(FirebaseFirestore database)
-    {
-        this.db = database;
-    }
-
-    public override void InitDataBase(FirebaseFirestore database, string userId)
-    {
-        this.db = database;
-        this.userId = userId;
-        Debug.Log($"ResourcesSO initialized with userId: {userId}", this);
-    }
-
-    public override Task DeleteDataAsync()
-    {
-        return null;
-    }
-
-    public override async Task<DocumentSnapshot> UpdateFromServerAsync<T>()
-    {
-        var data = await db.Collection("Users").Document(userId).Collection("Resources").Document(DocumentId).GetSnapshotAsync();
-        energy = data.GetValue<int>("Energy");
-        coin = data.GetValue<int>("Coin");
-        jewel = data.GetValue<int>("Jewel");
-        return data;
-    }
-
-
-
-    public override async Task SetDataAsync(object data)
-    {
-        await db.Collection("Users").Document(userId).Collection("Resources").Document(DocumentId).SetAsync(data);
-
-    }
-
-    public override async Task UpdateDataAsync()
-    {
-        Dictionary<string, object> updates = new Dictionary<string, object>
-        {
-            { "Energy", energy },
-            { "Coin", coin },
-            { "Jewel", jewel }
-        };
-        await db.Collection("Users").Document(userId).Collection("Resources").Document(DocumentId).UpdateAsync(updates);
-        //throw new System.NotImplementedException();
-    }
-
-    
-
-    public override async Task<DocumentSnapshot> UpdateFromServerAsync(bool updateAll)
-    {
-        var data = await db.Collection("Users").Document(userId).Collection("Resources").Document(DocumentId).GetSnapshotAsync();
-        energy = data.GetValue<int>("Energy");
-        coin = data.GetValue<int>("Coin");
-        jewel = data.GetValue<int>("Jewel");
-        return data;
-    }
-
 }
--- NyangNyangStar/Assets/Data/SO/FireStore/Users/UsersSO.cs
+++ NyangNyangStar/Assets/Data/SO/FireStore/Users/UsersSO.cs
@@ -1,16 +1,14 @@
 using Firebase.Firestore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
+[FirestorePath("Users/{userId}")]
 [CreateAssetMenu(fileName = "UsersSO", menuName = "ScriptableObjects/UsersSO")]
-
 public class UsersSO : BaseFireStore
 {
-
-    [SerializeField] private string userId;
-    [SerializeField] private string nickname;
+    [FirestoreField("UserID")] [SerializeField] private string userId;
+    [FirestoreField("Name")]   [SerializeField] private string nickname;
 
     [SerializeField] public int userLevel = 1;
     [SerializeField] public int userExp = 0;
@@ -18,7 +16,7 @@ public class UsersSO : BaseFireStore
     [SerializeField] public int catLikeExp = 0;
 
     [SerializeField] public int uploadPost = 0;
-    [SerializeField] public int follwerNumber = 0;
+    [SerializeField] public int followerNumber = 0; 
     [SerializeField] public int followingNumber = 0;
     [SerializeField] public int lastLogin = 0;
     [SerializeField] public int lastLogout = 0;
@@ -27,126 +25,10 @@ public class UsersSO : BaseFireStore
     [SerializeField] private List<BaseFireStore> subCollections;
 
 
-    public override Task CreateNew(FirebaseFirestore database)
-    {
-        throw new System.NotImplementedException();
-    }
     public override async Task CreateNew(FirebaseFirestore database, string userId)
     {
-        this.db = database;
         this.userId = userId;
-        this.nickname = "New User";
-
-
-        foreach (var sub in subCollections)
-        {
-            await sub.CreateNew(database, userId);
-        }
-
-        Dictionary<string, object> updates = new Dictionary<string, object>
-        {
-            { "UserID", userId },
-            { "Name", nickname },
-            { "UserLevel", 1 },
-            { "UserExp", 0 },
-            { "CatLikeLevel", 1 },
-            { "CatLikeExp", 0 },
-            { "UploadPost", 0 },
-            { "FollowerNumber", 0 },
-            { "FollowingNumber", 0 },
-            { "LastLogin", 0 },
-            { "LastLogout", 0 }
-
-        };
-
-        await SetDataAsync(updates);
-
-
+        nickname = "New User";
+        await base.CreateNew(database, userId);
     }
-    public override void InitDataBase(FirebaseFirestore database)
-    {
-        this.db = database;
-    }
-
-    public override void InitDataBase(FirebaseFirestore database, string userId)
-    {
-        this.db = database;
-        this.userId = userId;
-        foreach (var sub in subCollections)
-        {
-           sub.InitDataBase(database, userId);
-        }
-
-    }
-
-    public override Task DeleteDataAsync()
-    {
-        return null;
-    }
-
-    public override async Task<DocumentSnapshot> UpdateFromServerAsync(bool updateAll)
-    {
-        var data = await db.Collection("Users").Document(userId).GetSnapshotAsync();
-        nickname = data.GetValue<string>("Name");
-        userId = data.GetValue<string>("UserID");
-
-        if(updateAll)
-        {
-            foreach (var sub in subCollections)
-            {
-                await sub.UpdateFromServerAsync(true);
-            }
-        }
-        return data;
-    }
-
-
-
-    public override async Task SetDataAsync(object data)
-    {
-        await db.Collection("Users").Document(userId).SetAsync(data);
-
-    }
-
-    public override async Task UpdateDataAsync()
-    {
-        Dictionary<string, object> updates = new Dictionary<string, object>
-        {
-            { "Name", nickname },
-            { "UserID", userId }
-        };
-        await db.Collection("Users").Document(userId).UpdateAsync(updates);
-
-    }
-
-    public override async Task UpdateSubToServerAsync<T>()
-    {
-        bool found = false;
-        foreach (var sub in subCollections.OfType<T>())
-        {
-            await sub.UpdateDataAsync();
-            found = true;
-        }
-        if (!found)
-            Debug.LogError($"No sub-collection of type {typeof(T).Name} found.", this);
-    }
-
-    public override async Task UpdateSubToServerAsync<T>(string documentId)
-    {
-        bool found = false;
-        foreach (var sub in subCollections.OfType<T>().Where(s => s.DocumentId == documentId))
-        {
-            await sub.UpdateDataAsync();
-            found = true;
-        }
-        if (!found)
-            Debug.LogError($"No '{typeof(T).Name}' with id '{documentId}'.", this);
-    }
-
-
-    public override Task<DocumentSnapshot> UpdateFromServerAsync<T>()
-    {
-        return null;
-    }
-
 }
