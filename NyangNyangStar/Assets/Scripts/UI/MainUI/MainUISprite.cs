da68b94d19c8af5b043cbff726edf4a7df275877
Jo Kyeongmin <skykm0405@naver.com>
2026-06-05 17:49:56 +0900

    [update] MainUI 배경 이미지 추가
     MainUI 배경 이미지 추가

--- NyangNyangStar/Assets/Scripts/UI/MainUI/MainUISprite.cs
+++ NyangNyangStar/Assets/Scripts/UI/MainUI/MainUISprite.cs
@@ -24,6 +24,7 @@ public class MainUISprite : UIBase
 
     private void SetSprites()
     {
+        SetSprite(MainUIImages.Background, "Main_BG");
         SetSprite(MainUIImages.ShopButton, "Main_Btn_Shop");
         SetSprite(MainUIImages.EventButton, "Main_Btn_Event_Scratching");
         SetSprite(MainUIImages.DailyCheckInButton, "Main_Btn_Attendance");
@@ -63,6 +64,7 @@ public class MainUISprite : UIBase
 
 public enum MainUIImages
 {
+    Background,                // 배경
     ShopButton,                // 상점
     EventButton,               // 시즌 이벤트
     DailyCheckInButton,        // 출석 체크
