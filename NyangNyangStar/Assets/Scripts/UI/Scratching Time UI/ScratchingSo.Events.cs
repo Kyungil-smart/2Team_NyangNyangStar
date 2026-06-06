0decf9e6b63cef0bbf8fe45e56cf915ebcc920fb
제갈도원 <insopa01@naver.com>
2026-06-06 19:33:42 +0900

    refactor : 스크래칭 타임 코드 분리

--- NyangNyangStar/Assets/Scripts/UI/Scratching Time UI/ScratchingSo.Events.cs
+++ NyangNyangStar/Assets/Scripts/UI/Scratching Time UI/ScratchingSo.Events.cs
@@ -4,11 +4,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
 {
     public partial class ScratchingSo
     {
-        /// <summary>
-        /// 지정한 스테이지의 내구도 변경 이벤트에 리스너를 등록합니다.
-        /// </summary>
-        /// <param name="stage">리스너를 등록할 스테이지 값</param>
-        /// <param name="listener">등록할 리스너</param>
+        // 지정한 스테이지의 내구도 변경 이벤트에 리스너를 등록
         public void AddDamagedListener(int stage, Action listener)
         {
             if (!IsContainsKey(stage))
@@ -17,11 +13,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
             _dataDict[stage].OnScratcherDamaged += listener;
         }
 
-        /// <summary>
-        /// 지정한 스테이지의 파괴 이벤트에 리스너를 등록합니다.
-        /// </summary>
-        /// <param name="stage">리스너를 등록할 스테이지 값</param>
-        /// <param name="listener">등록할 리스너</param>
+        // 지정한 스테이지의 파괴 이벤트에 리스너를 등록
         public void AddDestroyedListener(int stage, Action listener)
         {
             if (!IsContainsKey(stage))
@@ -30,10 +22,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
             _dataDict[stage].OnScratcherDestroyed += listener;
         }
 
-        /// <summary>
-        /// 지정한 스테이지의 내구도 변경 이벤트 리스너를 모두 제거합니다.
-        /// </summary>
-        /// <param name="stage">리스너를 제거할 스테이지 값</param>
+        // 지정한 스테이지의 내구도 변경 이벤트 리스너를 모두 제거
         public void RemoveAllDamagedListener(int stage)
         {
             if (!IsContainsKey(stage))
@@ -42,10 +31,7 @@ namespace Data.ScriptableObjects.ScratchingTimeSO
             _dataDict[stage].OnScratcherDamaged = null;
         }
 
-        /// <summary>
-        /// 지정한 스테이지의 파괴 이벤트 리스너를 모두 제거합니다.
-        /// </summary>
-        /// <param name="stage">리스너를 제거할 스테이지 값</param>
+        // 지정한 스테이지의 파괴 이벤트 리스너를 모두 제거
         public void RemoveAllDestroyedListener(int stage)
         {
             if (!IsContainsKey(stage))
