9226e045a3f6994899880b6b2c69008cbb95b6de
Hooni0_0 <startrack0515@gmail.com>
2026-06-06 18:40:17 +0900

    [feat] 머지 보드 열기 컨트롤러 추가
    - MergeBoardController 추가
    - 보드 창 열기 및 닫기 기능 추가
    - 보드 오픈 시 MergeBoardLoader를 통해 서버 데이터 로드 처리 추가
    - 보드 닫기 시 UpDownScreenController 하강 애니메이션 호출 처리 추가
    - 보드 재오픈 시 강제 리로드 상태 초기화 기능 추가

---
+++ NyangNyangStar/Assets/Scripts/UI/MergeBoard/MergeBoardController.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading.Tasks;
+using Data.ScriptableObjects.MergeBoard;
+using UI.MainUI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.MergeBoard
+{
+    public class MergeBoardController : MonoBehaviour
+    {
+        [Header("보드 창")]
+        [SerializeField] private GameObject _boardRoot;
+        [SerializeField] private MergeBoardLoader _mergeBoardLoader;
+        [SerializeField] private Button _closeButton;
+
+        [Header("옵션")]
+        [SerializeField] private bool _reloadOnOpen;
+
+        private UpDownScreenController _upDownCon;
+
+        public bool IsOpen;
+
+        private void OnEnable()
+        {
+            _closeButton.onClick.AddListener(CloseBoard);
+        }
+
+        private void Start()
+        {
+            _upDownCon = FindObjectOfType<UpDownScreenController>();
+        }
+
+        private void OnDisable()
+        {
+            _closeButton.onClick.RemoveListener(CloseBoard);
+        }
+
+        public async void OpenBoard()
+        {
+            await OpenBoardAsync();
+        }
+
+        private async Task OpenBoardAsync()
+        {
+            if (IsOpen)
+                return;
+
+            if (_boardRoot == null)
+            {
+                DebugTool.Warning("보드 창 Root가 연결되지 않았습니다.", DebugType.Board, this);
+                return;
+            }
+
+            IsOpen = false;
+
+            try
+            {
+                await Task.Yield();
+
+                if (_mergeBoardLoader == null)
+                    _mergeBoardLoader = _boardRoot.GetComponentInChildren<MergeBoardLoader>(true);
+
+                if (_mergeBoardLoader == null)
+                {
+                    DebugTool.Warning("MergeBoardLoader가 연결되지 않았습니다.", DebugType.Board, this);
+                    return;
+                }
+
+                await _mergeBoardLoader.LoadAsync(_reloadOnOpen);
+            }
+            finally
+            {
+                IsOpen = false;
+            }
+        }
+
+        public void CloseBoard()
+        {
+            if (_boardRoot == null)
+                return;
+            
+            if(_upDownCon == null)
+                _upDownCon = FindObjectOfType<UpDownScreenController>();
+            _upDownCon.DownAnimation();
+        }
+
+        public void ForceReloadOnNextOpen()
+        {
+            if (_mergeBoardLoader != null)
+                _mergeBoardLoader.ResetLoadedState();
+        }
+    }
+}
