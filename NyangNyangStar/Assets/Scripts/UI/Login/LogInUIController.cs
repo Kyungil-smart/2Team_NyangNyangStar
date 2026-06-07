135af4687723aa8d242305385612c2d36701e4e9
Hooni0_0 <startrack0515@gmail.com>
2026-06-08 01:14:41 +0900

    [feat] 로그인 화면 전환 애니메이션 추가
    - LogInUIController 추가
    - 로그인 UI 로고 이미지 Native Size 설정 처리 추가
    - 로그인 화면에서 UpDownImage 하강 애니메이션 추가
    - 전환 애니메이션 완료 후 다음 씬으로 이동 처리 추가
    - DOTween 기반 로그인 씬 전환 연출 추가

---
+++ NyangNyangStar/Assets/Scripts/UI/Login/LogInUIController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Managers;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Login
+{
+    public class LogInUIController : MonoBehaviour
+    {
+        [SerializeField] private Image _logoImage;
+        [SerializeField] private Image _upDownImage;
+
+        private void Awake()
+        {
+            if(_logoImage != null)
+                _logoImage.SetNativeSize();
+        }
+
+        public void DownImage()
+        {
+            if (_upDownImage == null)
+                return;
+            
+            RectTransform rectTransform = _upDownImage.GetComponent<RectTransform>();
+            
+            rectTransform.anchoredPosition = new Vector2(0, 1920f);
+            rectTransform.DOAnchorPos(Vector2.zero, 0.5f).SetEase(Ease.OutQuad)
+                .OnComplete(GameManager.Scene.LoadNextScene);
+        }
+    }
+}
