5978e058d2b985704dcf2de5a55c67a7cc80fa76
Jo Kyeongmin <skykm0405@naver.com>
2026-06-17 12:50:41 +0900

    [feat] SelectedCatPopup 이미지 적용
    SelectedCatPopupSprite
    선택한 고양이 수첩 화면

---
+++ NyangNyangStar/Assets/Scripts/UI/PhotoCollection/SelectedCatPopupSprite.cs
@@ -0,0 +1,58 @@
+using System;
+using UI;
+using UI.Base;
+using UnityEngine.UI;
+
+public class SelectedCatPopupSprite : UIBase
+{
+    private UISpriteController[] _spriteController;
+
+    public override void Init()
+    {
+        Bind<Image>(typeof(SelectedCatPopupImages));
+
+        _spriteController = new UISpriteController[Enum.GetValues(typeof(SelectedCatPopupImages)).Length];
+
+        for (int i = 0; i < Enum.GetValues(typeof(SelectedCatPopupImages)).Length; i++)
+        {
+            _spriteController[i] = new UISpriteController(GetImage(i));
+        }
+
+        SetSprites();
+    }
+
+    private void SetSprites()
+    {
+        SetSprite(SelectedCatPopupImages.Panel, "Snap_Panel_Note");
+        SetSprite(SelectedCatPopupImages.BackButton, "Btn_Back");
+        SetSprite(SelectedCatPopupImages.CloseButton, "Btn_Close");
+        SetSprite(SelectedCatPopupImages.CatBackground, "Snap_BG");
+        SetSprite(SelectedCatPopupImages.CatImage, "Profile_ProfileImages_2");
+        SetSprite(SelectedCatPopupImages.CollectionButton, "Btn_Camera");
+    }
+
+    private void SetSprite(SelectedCatPopupImages image, string key)
+    {
+        _spriteController[(int)image].ChangeSprite(key);
+    }
+
+    private void OnDestroy()
+    {
+        if (_spriteController == null) return;
+
+        foreach (UISpriteController controller in _spriteController)
+        {
+            controller?.ReleaseSprite();
+        }
+    }
+}
+
+public enum SelectedCatPopupImages
+{
+    Panel,
+    BackButton,
+    CloseButton,
+    CatBackground,
+    CatImage,
+    CollectionButton,
+}
