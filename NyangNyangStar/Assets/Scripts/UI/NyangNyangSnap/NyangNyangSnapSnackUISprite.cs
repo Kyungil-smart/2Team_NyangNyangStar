ca89548e0828cef88f95e41732ee1d93aa3a883b
wanyong1 <chy7087114@gmail.com>
2026-07-08 10:42:41 +0900

    [fix] 냥냥스냅 UI/UX 개선

--- NyangNyangStar/Assets/Scripts/UI/NyangNyangSnap/NyangNyangSnapSnackUISprite.cs
+++ NyangNyangStar/Assets/Scripts/UI/NyangNyangSnap/NyangNyangSnapSnackUISprite.cs
@@ -28,8 +28,8 @@ public class NyangNyangSnapSnackUISprite : UIBase
     private void SetSprites()
     {
         SetSprite(NyangNyangSnapSnackUIImage.ScrollView, "Snap_Panel_ToolList");
-        SetSprite(NyangNyangSnapSnackUIImage.CloseButton, "Btn_Close", new Color32(0,0,0,255));
-        SetSprite(NyangNyangSnapSnackUIImage.SnackButton, "Shape_Rectangle");
+        SetSprite(NyangNyangSnapSnackUIImage.CloseButton, "Btn_Close");
+        SetSprite(NyangNyangSnapSnackUIImage.SnackButton, "Snap_Panel_Tool");
 
     }
 
--- NyangNyangStar/Assets/Scripts/UI/NyangNyangSnap/NyangNyangSnapToyUISprite.cs
+++ NyangNyangStar/Assets/Scripts/UI/NyangNyangSnap/NyangNyangSnapToyUISprite.cs
@@ -28,8 +28,8 @@ public class NyangNyangSnapToyUISprite : UIBase
     private void SetSprites()
     {
         SetSprite(NyangNyangSnapToyUIImage.ScrollView, "Snap_Panel_ToolList");
-        SetSprite(NyangNyangSnapToyUIImage.CloseButton, "Btn_Close", new Color32(0, 0, 0, 255));
-        SetSprite(NyangNyangSnapToyUIImage.ToyButton, "Shape_Rectangle");
+        SetSprite(NyangNyangSnapToyUIImage.CloseButton, "Btn_Close");
+        SetSprite(NyangNyangSnapToyUIImage.ToyButton, "Snap_Panel_Tool");
 
     }
 
